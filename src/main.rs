5071f6a9123dcad1d9c8b9ff6ef281051c76a840
shivaduke28 <shivaduke28@gmail.com>
2022-03-04 08:37:18 +0900

    normal

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 #![allow(dead_code)]
-use std::{fs::File, io::prelude::*};
 use rayon::prelude::*;
 
 mod rayt;
@@ -11,9 +10,31 @@ use crate::rayt::*;
 use image::{Rgb, RgbImage};
 
 fn color(ray: &Ray) -> Color {
+    let c = Float3::new(0.0, 0.0, -1.0);
+    let t = hit_sphere(c, 0.5, &ray);
+    if t > 0.0 {
+        let n = (ray.at(t) - c).normalize();
+        return (n + Float3::one()) * 0.5;
+    }
+
     let d = ray.direction.normalize();
     let t = 0.5 * (d.y() + 1.0);
     Color::new(0.5, 0.7, 1.0).lerp(Color::one(), t)
+}
+
+// dot(d, d) * t^2 + 2 * dot(d, c - o) * t + dot(c - o, c - o) = r^2
+fn hit_sphere(center: Float3, radius: f64, ray: &Ray) -> f64 {
+    let oc = ray.origin - center;
+    let d = ray.direction;
+    let a = d.length_squared();
+    let b = 2.0 * d.dot(oc);
+    let c = oc.length_squared() - radius * radius;
+    let d = b * b - 4.0 * a * c;
+    if d > 0.0 {
+        (-b - d.sqrt()) / (2.0 * a)
+    } else {
+        -1.0
+    }
 }
 
 fn main() {
@@ -28,7 +49,7 @@ fn main() {
         .par_iter_mut()
         .for_each(|(x, y, pixel)| {
             let u = *x as f64 / (IMAGE_WIDTH - 1) as f64;
-            let v = *y as f64 / (IMAGE_HEIGHT - 1) as f64;
+            let v = (IMAGE_HEIGHT - 1  - *y) as f64 / (IMAGE_HEIGHT - 1) as f64;
             let ray = camera.ray(u, v);
             let rgb = color(&ray).to_rgb();
             pixel[0] = rgb[0];
